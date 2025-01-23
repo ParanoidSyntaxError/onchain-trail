b5a538434d56af291d9ca33a6c20db472b8ac177
ParanoidSyntaxError <git@jbojtor.com>
2025-01-23 15:12:24 +0100

    Added start screen

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,34 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function HomePage() {
 	return (
-		<div
-			className="p-8"
-		>
-
+		<div>
+			<img
+				src="/title-bg.png"
+				className="absolute w-full bottom-0 -z-10"
+				style={{
+					imageRendering: "pixelated",
+				}}
+			/>
+			<div
+				className="text-center text-5xl px-4 mt-16"
+			>
+				Onchain Trail
+			</div>
+			<div
+				className="flex justify-center mt-16"
+			>
+				<Button
+					asChild
+				>
+					<Link
+						href="/camp"
+					>
+						Start
+					</Link>
+				</Button>
+			</div>
 		</div >
 	);
 }
